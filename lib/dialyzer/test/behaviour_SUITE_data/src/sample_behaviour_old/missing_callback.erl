1fb05fe1f546a8519e667d90915a5cb7e56907ae
Al <38429047+albsch@users.noreply.github.com>
2024-05-24 14:58:17 +0200

    dialyzer: test fix comment in missing_callback.erl
    Incorrect module

--- lib/dialyzer/test/behaviour_SUITE_data/src/sample_behaviour_old/missing_callback.erl
+++ lib/dialyzer/test/behaviour_SUITE_data/src/sample_behaviour_old/missing_callback.erl
@@ -1,9 +1,10 @@
-%%% This is a correct callback module for the correct_behaviour.
+%%% This is an incorrect callback module for the correct_behaviour.
 
 -module(missing_callback).
 
 -behaviour(correct_behaviour).
 
+% missing foo/0 callback
 -export([bar/2]).
 
 bar({'reply', _Any}, []) ->
